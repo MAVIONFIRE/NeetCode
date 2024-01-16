d5e68a79299206972cebf92892bbf8f63a5ef910
MAVIONFIRE <105316791+MAVIONFIRE@users.noreply.github.com>
2024-01-16 07:51:31 -0500

    Started LeetCode 242

---
+++ 242-Valid-Anagram/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _242_Valid_Anagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
